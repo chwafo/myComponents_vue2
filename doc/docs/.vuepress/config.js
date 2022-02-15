89e6be7f12a2d58c084b2fb96c60ef7c318ff94a
李珂 <15671629634@163.com>
2022-02-15 14:20:28 +0800

    修改文档路径

--- doc/docs/.vuepress/config.js
+++ doc/docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
   dest: path.resolve(__dirname, '../../../docs'),
-  base: "/myComponents_vue2.github.io/docs/",
+  base: "/",
   markdown: {
     title: '99999',
     description: '88888',
